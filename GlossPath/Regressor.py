bfc42cb5c2c28358c38be7755d0300335e81e018
Tamjeed Azad <tamjazad@users.noreply.github.com>
2024-09-27 16:16:05 -0400

    adding some initial package files

---
+++ GlossPath/Regressor.py
@@ -0,0 +1,6 @@
+from skglm import GroupLasso
+
+class Regressor():
+    def __init__(self):
+        pass
+    
---
+++ GlossPath/pathways.py
@@ -0,0 +1,10 @@
+## basically a class that holds the pathway acquisition logic
+
+class PathwaysModule():
+    def __init__(self):
+        pass
+    def HallmarkPathways(self):
+        pass
+    def KEGGPathways(self):
+        pass 
+
