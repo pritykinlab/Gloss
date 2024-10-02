6de88d69b2bce2f14baf8b08b20998eb0366fb3b
tamjazad <tamjazad@users.noreply.github.com>
2024-10-02 15:48:44 -0400

    bunch of logic using GridSearchCV for cross validation now

--- GlossPath/RegressCV.py
+++ GlossPath/RegressCV.py
@@ -3,91 +3,44 @@
 
 import Regressor
 import PathwaysModule
-from sklearn.model_selection import KFold
+import PrepData
+
+from sklearn.model_selection import KFold, GridSearchCV, cross_validate
 from sklearn.metrics import make_scorer, r2_score, mean_squared_error
 import numpy as np
 from scipy.stats import pearsonr
 from skglm import GroupLasso
 
 class RegressCV():
-    def __init__(self):
-        pass
+    # in init, I need to pass in the regressions as 
+    def __init__(self, adata, resolutions, pathway_string,
+                 alphas = [0.004, 0.005, 0.006, 0.007], l1s = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5]):
+        self.adata = adata
+        self.alphas = alphas 
+        self.l1s = l1s
+        self.resolutions = resolutions
+        self.pathway_string = pathway_string
 
-    def _cv_loop(X, y, partitions_weights, alphas):
+    def _cv_loop(self, X, y, partitions_weights, alphas):
         
         # Set up outer cross-validation (for model evaluation)
         outer_cv = KFold(n_splits=5, shuffle=True, random_state=42)
         # Set up inner cross-validation (for hyperparameter tuning)
         inner_cv = KFold(n_splits=5, shuffle=True, random_state=42)
         
-        # Loop over outer folds
-        estimators = []
+        for res in self.resolutions:
+            for ctype in self.resolutions[res]:
 
-        test_mse_arr = []
-        train_mse_arr = []
-        train_pearson_arr = []
-        test_pearson_arr = []
-        train_r2_score_arr = []
-        test_r2_score_arr = []
+                # need to throw in some data preparation logic here using PrepData
 
-        for train_index, test_index in outer_cv.split(X):
-            X_train, X_test = X[train_index], X[test_index]
-            y_train, y_test = y[train_index], y[test_index]
+                myreg = Regressor(gene_list = self.adata.var.index, pathway_string='hallmark')
+                param_grid = {
+                    'group_reg' : self.alphas,
+                    'l1_reg' : self.l1s
+                }
+                grid_search = GridSearchCV(myreg, param_grid, cv=inner_cv, n_jobs=-1)
+                response = cross_validate(grid_search, X, y, cv=outer_cv, n_jobs=-1)
 
-            best_alpha = None
-            best_mse = np.inf  # Keep track of the best score
-
-            # Loop over possible alpha values (hyperparameter tuning)
-            for alpha in alphas:
-                print(alpha)
-                inner_mse = []  # Store validation MSE for each inner fold
-
-                for inner_train_index, inner_val_index in inner_cv.split(X_train):
-                    X_inner_train, X_inner_val = X_train[inner_train_index], X_train[inner_val_index]
-                    y_inner_train, y_inner_val = y_train[inner_train_index], y_train[inner_val_index]
-
-                    # Initialize and fit the Group Lasso model with current alpha
-                    model = GroupLasso(alpha=alpha, groups=partitions_weights[0], weights=partitions_weights[1])
-                    model.fit(X_inner_train, y_inner_train)
-
-                    # Predict on the validation set
-                    y_pred_inner = model.predict(X_inner_val)
-                    mse_inner = mean_squared_error(y_inner_val, y_pred_inner)
-                    inner_mse.append(mse_inner)
-
-                # Calculate the average MSE across the inner folds for this alpha
-                avg_inner_mse = np.mean(inner_mse)
-
-                # Check if this is the best alpha so far
-                if avg_inner_mse < best_mse:
-                    best_mse = avg_inner_mse
-                    best_alpha = alpha
-
-            # After hyperparameter tuning, train on the full training set using the best alpha
-            best_model = GroupLasso(alpha=best_alpha, groups=partitions_weights[0], weights=partitions_weights[1])
-            best_model.fit(X_train, y_train)
-            estimators.append(best_model)
-
-            # Evaluate on the outer test set
-            y_pred_test = best_model.predict(X_test)
-            test_mse_arr.append(mean_squared_error(y_test, y_pred_test))
-            test_r2_score_arr.append(r2_score(y_test, y_pred_test))
-            test_pearson_arr.append(pearsonr(y_test, y_pred_test)[0])
-
-            # Evaluate on the inner train set
-            y_pred_train = best_model.predict(X_train)
-            train_mse_arr.append(mean_squared_error(y_train, y_pred_train))
-            train_r2_score_arr.append(r2_score(y_train, y_pred_train))
-            train_pearson_arr.append(pearsonr(y_train, y_pred_train)[0])   
-        
-        res = {
-            'estimator' : estimators,
-            'train_mse' : train_mse_arr,
-            'train_pearson' : train_pearson_arr,
-            'train_r2_score' : train_r2_score_arr,
-            'test_mse' : test_mse_arr,
-            'test_pearson' : test_pearson_arr,
-            'test_r2_score' : test_r2_score_arr,
-        }
+                # what to return, what to return
         
         return res
